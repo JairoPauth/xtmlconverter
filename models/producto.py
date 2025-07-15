f017004df0bce5e1a0af1aa28684d72f8d369d09
Jairo Pauth <Jairo.pauth@bldr.com>
2025-07-15 15:19:48 -0600

    test sdf

--- models/producto.py
+++ models/producto.py
@@ -2,17 +2,19 @@
 class ProductoSeleccionado:
     def __init__(self, codigo: str, descripcion_base: str, unidad: str, cantidad: int):
         self.codigo = codigo
-        self.descripcion = f"{descripcion_base} ({unidad})"
-        self.cantidad = cantidad
         self.unidad = int(unidad)
+        self.cantidad = cantidad
+
+        # Agregar longitud solo a productos que NO son LVL
+        if "LVL" in descripcion_base.upper():
+            self.descripcion = descripcion_base  # sin unidad
+        else:
+            self.descripcion = f"{descripcion_base} ({unidad})"
 
     def to_dict(self):
         return {
             "codigo": self.codigo,
             "descripcion": self.descripcion,
             "cantidad": self.cantidad,
-            "unidad": self.unidad 
-
-
-             
+            "unidad": self.unidad
         }
